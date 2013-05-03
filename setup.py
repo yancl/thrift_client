893da23313c37cb31a2bccc2348255f528939154
yancl <kmoving@gmail.com>
2013-05-03 12:06:30 +0800

    add publish for setup

--- setup.py
+++ setup.py
@@ -1,12 +1,31 @@
 #!/usr/bin/env python
+import os
+import sys
+from setuptools import setup
 
-from distutils.core import setup
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload -r internal')
+    sys.exit()
 
 setup(
         name = "thrift_client",
         version = "0.10",
         description="python thrift client supports failover",
-        maintainer="yancl",
-        maintainer_email="kmoving@gmail.com",
+        long_description=open("README.md").read(),
+        author="yancl",
+        author_email="kmoving@gmail.com",
+        url='https://github.com/yancl/thrift_client',
+        classifiers=[
+            'Programming Language :: Python',
+        ],
+        platforms='Linux',
+        license='MIT License',
+        zip_safe=False,
+        install_requires=[
+            'thrift',
+        ],
+        tests_require=[
+            'nose',
+        ],
         packages=['thrift_client', 'thrift_client.client', 'thrift_client.client.connection']
 )
